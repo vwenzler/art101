ce44b94695354a0c24b3761c807df1e88a1f92b4
vwenzler <91237499+vwenzler@users.noreply.github.com>
2021-10-18 21:11:33 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ * Author: Vincent Wenzler
+ * Created: 18 October
+ *License: Public Domain
+ */
+
+ // Define Variables
+ make = "Volvo";
+ model = "V70"
+ color = "Silver";
+ year = "2000";
+
+ // calculate
+ age = 2021 - year;
+
+ // output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age:" + age + " years<br>");
