600aeb7b7b035f5077be92d518c01f3d3b2adbcb
vwenzler <91237499+vwenzler@users.noreply.github.com>
2021-10-25 21:46:29 -0700

    lab 7 stuff

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -2,25 +2,31 @@
  * Author: Vincent Wenzler
  *Created: 20 October
  *License: Public Domain
+ *Based on code from Wes Modes <wmodes@csumb.edu>
  */
 
 
 // removenametype1 - a function that removes everything but the first 3 letters in a name
-  function removenametype1(){
-    var name = window.prompt("your name seems to have a bit too much meaning");
-    console.log("normalName =", normalName);
-    //split string into an Array
-    var namearray = normalName.split('');
-    console.log("nameArray =", nameArray);
-    //sorts the array
-    var nameArraySort = nameArray.sort();
-    console.log("nameArraySlice =", nameArraySlice);
-    //join array into a string again
-    var sortedname = NameArraySort.join('');
-    console.log("shortname =", shortname);
-    return shortname;
+  function fixname() {
+    // prompts for name
+    var normalname = window.prompt("Your name seems to have a bit too much meaning. Let me fix that.") ;
+    console.log("normalname =", normalname) ;
+
+    // split string into an Array
+    var nameArray = normalname.split('') ;
+    console.log("nameArray =", nameArray) ;
+
+    // sorts the array
+    var nameArraySort = nameArray.sort() ;
+    console.log("nameArraySort =", nameArraySort) ;
+
+    // join array into a string again
+    var sortedName = nameArraySort.join('') ;
+    console.log("sortedName =", sortedName) ;
+    // these could be wrtitten on the same line
+    return sortedName;
   }
 
 //Output
-document.writeln("There we go, your name is finally short enough: ",
-    removenametype1(), "</br>");
+document.writeln("There we go, your name finally doesn't make sense: ",
+    fixname(), "</br>") ;
