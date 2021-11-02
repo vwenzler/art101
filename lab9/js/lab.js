19656bd16cff9681e3e03b722fec5b96a06d79a2
vwenzler <91237499+vwenzler@users.noreply.github.com>
2021-11-01 23:05:45 -0700

    fixed lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,11 +1,28 @@
 /*
  * Author: Vincent Wenzler
- *Created: 20 October
+ *Created: 1 November 2021
  *License: Public Domain
  */
 
-var outputEl = document.getElementById("output");
+//find div "output" Assign variable
+var outputEl = document.getElementById('output');
 
-var newEl = document.createElement"("div");
+//create new element assign a vatiable
+var new1El = document.createElement("h1");
 
-outputEl.appendChild(newEl);
+//creat parent node
+var head1 = document.createTextNode("Trust me this is supposed to be here...");
+
+//append child node to parent
+new1El.appendChild(head1);
+outputEl.appendChild(new1El);
+
+var new2El = document.createElement("p");
+
+var textline = document.createTextNode("Really, its just the output of my javascript code");
+
+new2El.appendChild(textline);
+outputEl.appendChild(new2El);
+
+new1El.style.color = "#E5CB3E";
+new2El.style.color = "#149390"
