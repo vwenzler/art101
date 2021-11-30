391f94b89108173bec43157f5940098e5a9d20e3
vwenzler <91237499+vwenzler@users.noreply.github.com>
2021-11-29 23:55:25 -0800

    stuff

---
+++ lab15/js/lab.js
@@ -0,0 +1,35 @@
+/*
+ * Author: Vincent Wenzler, Katana Parker
+ *Created: 29 November 2021
+ *License: Public Domain
+ *Basic structure from Wes Modes wmodes@ucsc.edu
+ */
+
+var myUrl = "http://numbersapi.com/11/30/date";
+
+
+function callAjax() {
+  console.log("click");
+  // Using the core $.ajax() method that Wes showed us in lecture
+  $.ajax({
+      // API endpoint
+      url: myUrl,
+      // GET request
+      type: "GET",
+      // data type
+  })
+  // If the request succeeds
+  // data is passed back
+  .done(function(data) {
+      console.log("Success:", data);
+      $("#output").html(JSON.stringify(data));
+      })
+
+  // If the request fails
+  .fail(function(request,error) {
+      console.log(request, error);
+	})
+
+}
+
+$("#activate").click(callAjax);
